148bd42664f9fc673cdd10942d8d99266131f8eb
Lee Chilvers <lee.chilvers@icloud.com>
2021-12-12 14:53:46 +0000

    comments

--- src/main/java/com/shutl/Application.java
+++ src/main/java/com/shutl/Application.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+    //run to start api
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
